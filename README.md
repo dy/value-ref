c6a1bd09ee8ff29ccb79dee55feb096be76cf19e
dy <df.creative@gmail.com>
2022-01-13 10:41:47 -0500

    Init package

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # vref
- Observable value reference
+
+| Observable value reference.
+
+_`let ref = v( init? )`_
+
+Creates reactive mutable _`ref`_ object with a single `.value` property holding internal value.
+Identical to [vue3 ref](https://v3.vuejs.org/api/refs-api.html#ref) with _Observable_ and _AsyncIterable_ interface.
+Ex `spect/v.js`.
+
+```js
+import v from 'vref.js'
+
+let count = v(0)
+count.value // 0
+
+count.subscribe(value => {
+  console.log(value)
+  return () => console.log('teardown', value)
+})
+count.value = 1
+// > 1
+count.value = 2
+// > "teardown" 1
+// > 2
+
+let double = count.map(value => value * 2) // create mapped ref
+double.value // 4
+
+count.value = 3
+double.value // 6
+
+let sum = v(count.value + double.value)
+count.subscribe(v => sum.value = v + double.value)
+double.subscribe(v => sum.value = count.value + v)
+
+// async iterable
+for await (const value of sum) console.log(value)
+
+sum.dispose() // destroy observations
+```
+
+## Related
+
+* [subscribable-things](https://github.com/chrisguttandin/subscribable-things) − collection of observables for different browser APIs - perfect match with spect.
+
+## References
+
+* [vue3/ref](https://v3.vuejs.org/api/refs-api.html)
+* [knockout/observable](https://github.com/knockout/tko/issues/22)
+* [mobx/observable](https://mobx.js.org/api.html)
+* [rxjs](https://ghub.io/rxjs)
+* [observable](https://ghub.io/observable)
+* [observable proposal](https://github.com/tc39/proposal-observable)
+* [observ](https://ghub.io/observ)
+* [mutant](https://ghub.io/mutant)
+* [iron](https://github.com/ironjs/iron)
+* [icaro](https://ghub.io/icaro)
+* [introspected](https://ghub.io/introspected)
+* [unihooks](https://ghub.io/unihooks)
+* [augmentor](https://ghub.io/augmentor) and others.
+
+<p align="center">ॐ</p>
---
+++ vref.js
@@ -0,0 +1,65 @@
+export default init => new Ref(init)
+
+const _teardown = Symbol()
+
+class Ref {
+  #observers=[]
+
+  constructor(init){ this[0] = init }
+
+  get value() { return this[0] }
+  set value(val) {
+    this[0] = val
+    for (let sub of this.#observers) {
+      if (typeof sub[_teardown] === 'function') sub[_teardown]()
+      if (sub.next) (sub[_teardown] = sub.next(val))
+    }
+  }
+
+  valueOf() {return this.value}
+  toString() {return this.value}
+  [Symbol.toPrimitive](hint) {return this.value}
+
+  // FIXME: replace with 0b?
+  subscribe(next, error, complete) {
+    next = next && next.next || next
+    error = next && next.error || error
+    complete = next && next.complete || complete
+
+    const unsubscribe = () => (
+      this.#observers.length && this.#observers.splice(this.#observers.indexOf(subscription) >>> 0, 1),
+      complete && complete()
+    ),
+    subscription = { next, error, complete, unsubscribe }
+    this.#observers.push(subscription)
+
+    if ( this[0] !== undefined ) subscription[_teardown] = next(this[0])
+
+    return unsubscribe.unsubscribe = unsubscribe
+  }
+
+  map(mapper) {
+    const ref = new Ref()
+    this.subscribe(v => ref.value = mapper(v))
+    return ref
+  }
+
+  error(e) {this.#observers.map(sub => sub.error && sub.error(e))}
+
+  [Symbol.observable](){return this}
+
+  async *[Symbol.asyncIterator]() {
+    let resolve, buf = [], p = new Promise(r => resolve = r),
+      unsub = this.subscribe(v => ( buf.push(v), resolve(), p = new Promise(r => resolve = r) ))
+    try { while (1) yield* buf.splice(0), await p }
+    catch {}
+    unsub()
+  }
+
+  [Symbol.dispose]() {
+    this[0] = null
+    const unsubs = this.#observers.map(sub => ((typeof sub[_teardown] === 'function') && sub[_teardown](), sub.unsubscribe))
+    this.#observers.length = 0
+    unsubs.map(unsub => unsub())
+  }
+}
